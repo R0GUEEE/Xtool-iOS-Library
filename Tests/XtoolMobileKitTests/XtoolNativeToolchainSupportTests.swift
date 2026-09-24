3261f4d82a7d1147f2e3380db8f9761132f1df5b
R0GUEEE <41440185+R0GUEEE@users.noreply.github.com>
2026-09-23 23:23:19 -0400

    Test all native toolchain capabilities

--- Tests/XtoolMobileKitTests/XtoolNativeToolchainSupportTests.swift
+++ Tests/XtoolMobileKitTests/XtoolNativeToolchainSupportTests.swift
@@ -6,6 +6,14 @@ func nativeToolchainSupportInitializes() {
     let capabilities = XtoolNativeToolchainSupport.initialize()
 
     #expect(
+        capabilities.hasSwiftFrontend
+            == XtoolNativeToolchainSupport.hasEmbeddedSwiftFrontend
+    )
+    #expect(
+        capabilities.hasClang
+            == XtoolNativeToolchainSupport.hasEmbeddedClang
+    )
+    #expect(
         capabilities.hasLLDMachO
             == XtoolNativeToolchainSupport.hasEmbeddedLLDMachO
     )
